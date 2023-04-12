77be1698c89378e93300fd3fe8bb4c8deb23fcd1
max <68344959+codewmax@users.noreply.github.com>
2023-04-13 00:17:00 +0200

    Next Config adjusted.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig*/
 
-module.exports = {
+/*module.exports = {
   distDir: 'production',
-}
+}*/

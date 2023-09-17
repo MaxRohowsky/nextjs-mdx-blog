730948a1b6fccc5838b1d443bdc97240e0ab24df
max <68344959+codewmax@users.noreply.github.com>
2023-09-17 02:00:23 +0200

    Pagination beginnings

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -3,32 +3,96 @@ import styles from '../styles/Courses.module.scss'
 import { gql } from "@apollo/client";
 import { getApolloClient } from '../components/client';
 import Head from "next/head";
+import { useState } from "react";
 
-export default function Courses({ catedata }) {
+export default function Courses({ catedata, pageData }) {
+
+  
+  let startCursor = pageData.startCursor
+  //console.log(pageData)
+
+  const first = 4
+  //const [first, setFirst] = useState(4);
+  const [after, setAfter] = useState(pageData.startCursor);
+  const [data, setCateData] = useState(catedata);
+
+
+
+  async function handleClick() {
+    //setFirst(10);  // Change the value of first
+    const apolloClient = getApolloClient();
+    const { data } = await apolloClient.query({
+      query: gql`
+          query getPosts($after: String, $first: Int = null) {
+            categories(first: $first, after: $after) {
+              pageInfo {
+                endCursor
+                startCursor
+                hasNextPage
+                hasPreviousPage
+              }
+              nodes {
+                id
+                name
+                slug
+                description
+                categoryImages {
+                  categoryImage {
+                    sourceUrl
+                  }
+                }
+                courses(where: {orderby: {field: MENU_ORDER, order: ASC}}, first: 1) {
+                  edges {
+                    node {
+                      id
+                      uri
+                      title
+                      slug
+                      menuOrder
+                      link
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+      variables: {
+        first: first, // Use the updated first value
+        after: after, // Use the updated after value
+      },
+    });
+
+    setAfter(data.categories.pageInfo.endCursor);
+    setCateData(data.categories.nodes);
+    console.log(data.categories)
+  }
+
 
   var items = []
 
   // This for loop goes through all the categories
-  for (var i = 0; i < catedata.length; i++) {
-    if (catedata[i].courses.edges.length > 0 && !(catedata[i].name == 'Uncategorized')) { // category > one child and not uncategorized -> create card.
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].courses.edges.length > 0 && !(catedata[i].name == 'Uncategorized')) { // category > one child and not uncategorized -> create card.
       items.push(
         <Card
-          key= {i}
-          title={catedata[i].name}
-          img={catedata[i].categoryImages.categoryImage.sourceUrl}
-          body={catedata[i].description}
-          link={catedata[i].courses.edges[0].node.uri} // courses/category/lesson e.g. courses/pycharm/pycharm-basics
+          key={i}
+          title={data[i].name}
+          img={data[i].categoryImages.categoryImage.sourceUrl}
+          body={data[i].description}
+          link={data[i].courses.edges[0].node.uri} // courses/category/lesson e.g. courses/pycharm/pycharm-basics
         />
       )
     }
   }
 
 
+
   return (
     <>
       <Head>
         <title>Courses</title>
-        <meta name="description" content="Free Tech Tutorials"/>
+        <meta name="description" content="Free Tech Tutorials" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -41,6 +105,10 @@ export default function Courses({ catedata }) {
           {items}
         </div>
       </div>
+      <div>
+        <button onClick={handleClick}>Click me</button>
+      </div>
+
 
     </>
 
@@ -54,8 +122,14 @@ export async function getStaticProps() {
 
   const data = await apolloClient.query({
     query: gql`
-    {
-      categories {
+    query getPosts($after: String, $first: Int = null) {
+      categories(first: $first, after: $after) {
+        pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+          hasPreviousPage
+        }
         nodes {
           id
           name
@@ -81,14 +155,20 @@ export async function getStaticProps() {
         }
       }
     }
-    `
+    `,
+    variables: {
+      first: 4,
+      after: null
+    }
   });
 
-  const catedata = data.data.categories.nodes
+  let catedata = data.data.categories.nodes
+  const pageData = data.data.categories.pageInfo
 
   return {
     props: {
-      catedata
+      catedata,
+      pageData
     },
     revalidate: 10,
   }

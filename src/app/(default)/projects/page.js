8bbb7de946b26a6b9f8e739255eefeb6e8e45196
max <68344959+codewmax@users.noreply.github.com>
2024-04-15 01:38:43 +0200

    Refactor Apollo client queries in page.js and client.js

--- src/app/(default)/projects/page.js
+++ src/app/(default)/projects/page.js
@@ -1,68 +1,63 @@
 'use server'
-//import Card from "@/components/Card"
-//import styles from '../../styles/Courses.module.scss'
-//import Head from "next/head";
-
-
 import Client from './client';
-import { gql } from "@apollo/client";
-import { getApolloClient } from '../../../components/client';
 
 
 async function getCourses() {
-
-  const apolloClient = getApolloClient();
-
-  const data = await apolloClient.query({
-    query: gql`
-      query getPosts($before: String, $after: String, $first: Int = null, $last: Int = null) {
-        category(id: "dGVybToxNw=") {
-          name
-          children (first: $first, last: $last, before: $before, after: $after){
-            nodes {
-              name
-              slug
-              id
-              description
-              categoryImages {
-                categoryImage {
-                  sourceUrl
+  const response = await fetch(process.env.WORDPRESS_API, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: `
+        query getPosts($before: String, $after: String, $first: Int = null, $last: Int = null) {
+          category(id: "dGVybToxNw=") {
+            name
+            children (first: $first, last: $last, before: $before, after: $after){
+              nodes {
+                name
+                slug
+                id
+                description
+                categoryImages {
+                  categoryImage {
+                    sourceUrl
+                  }
                 }
-              }
-              courses(where: { orderby: { field: MENU_ORDER, order: ASC } }, first: 1) {
-                edges {
-                  node {
-                    id
-                    uri
-                    title
-                    slug
-                    menuOrder
-                    link
+                courses(where: { orderby: { field: MENU_ORDER, order: ASC } }, first: 1) {
+                  edges {
+                    node {
+                      id
+                      uri
+                      title
+                      slug
+                      menuOrder
+                      link
+                    }
                   }
                 }
               }
-            }
-            pageInfo {
-              endCursor
-              hasNextPage
-              hasPreviousPage
-              startCursor
+              pageInfo {
+                endCursor
+                hasNextPage
+                hasPreviousPage
+                startCursor
+              }
             }
           }
         }
-      }
-    `,
-    variables: {
-      first: 18, // Use the updated first value for pagination
-      after: null, // Use the updated after value for pagination
-    },
+      `,
+      variables: {
+        first: 18, // Use the updated first value for pagination
+        after: null, // Use the updated after value for pagination
+      },
+    }),
   });
 
-  const courses = data.data.category.children.nodes
+  const data = await response.json();
 
-  const pageInfo = data.data.category.children.pageInfo
+  const courses = data?.data?.category?.children?.nodes;
+  const pageInfo = data?.data?.category?.children?.pageInfo;
 
-  return { courses, pageInfo }
+  return { courses, pageInfo };
 }
 
 

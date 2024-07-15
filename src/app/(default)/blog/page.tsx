209e75a3f21a47ff18beceaf9a9ecb56e84465a1
max <68344959+codewmax@users.noreply.github.com>
2024-07-15 17:27:16 +0200

    chore: Update body class in layout.js and navbar.js for responsive design

--- src/app/(default)/blog/page.tsx
+++ src/app/(default)/blog/page.tsx
@@ -1,4 +1,5 @@
 
+import Particles from '@/components/dots/particles';
 
 
 import fs from 'fs';
@@ -49,16 +50,29 @@ export default function Overview() {
   return (
     <>
 
+      <div className='relative -z-10 w-0 h-0  top-[87px] left-[127px]'>
+        <Particles numberOfParticles={10} radius={60} opacity={0.2} color='black' left={0} />
+        <Particles numberOfParticles={30} radius={300} opacity={0.3} color='black' left={0} />
+        <Particles numberOfParticles={40} radius={380} opacity={0.2} color='black' left={0} />
+        <Particles numberOfParticles={50} radius={460} opacity={0.1} color='black' left={0} />
+        <Particles numberOfParticles={60} radius={540} opacity={0.05} color='black' left={0} />
+      </div>
 
-
-
+      <div className='flex justify-between  whitespace-nowrap text-slate-600 w-full h-40  text-5xl p-4 mb-6 overflow-hidden mt-8'  >
+        <span className="opacity-100 font-bold" /* style={{background: "url('uk-flag.gif')", backgroundSize:"cover", backgroundClip:"text", WebkitTextFillColor:"transparent", backgroundPosition: "center"}} */>Blog. </span>
+        <span className="opacity-10 hidden sm:block" >Blogue. </span>
+        <span className="opacity-10 hidden sm:block">ブログ. </span>
+        <span className="opacity-10 hidden md:block">블로그. </span>
+        <span className="opacity-10 hidden md:block">博客. </span>
+        <span className="opacity-10 hidden lg:block">Блог. </span>
+      </div>
 
       <div className='grid gap-1 grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))] '>
 
         {blogs.map((blog) => (
 
           <div key={blog.slug} className=" p-2 rounded-xl  w-full h-full bg-white dark:bg-slate-800">
-            <Link href={`/blg/${blog.slug}`} className="flex flex-col justify-between border border-gray-200 rounded-lg hover:shadow-lg transition-shadow duration-300 p-6 w-full h-full">
+            <Link href={`/blog/${blog.slug}`} className="flex flex-col justify-between border border-gray-200 rounded-lg hover:shadow-lg transition-shadow duration-300 p-6 w-full h-full">
               <h3 className="font-bold text-xl mb-2">{blog.meta.title}</h3>
               <p className="text-sm text-gray-500">{blog.meta.publishedOn}</p>
               <p className="py-4">{blog.meta.abstract}</p>
@@ -77,6 +91,5 @@ export default function Overview() {
 
       </div>
     </>
-
   )
 }
--- src/app/(default)/projects/page.js
+++ src/app/(default)/projects/page.js
@@ -1,6 +1,8 @@
 import Client from './client';
 import { projects } from '@/components/projects';
 import Link from 'next/link';
+import Image from 'next/image';
+import Particles from '@/components/dots/particles';
 
 export const metadata = {
   title: "Projects",
@@ -9,94 +11,51 @@ export const metadata = {
 
 // https://codepen.io/creativeocean/pen/JjemXGY
 
-async function getCourses() {
-  const response = await fetch(process.env.WORDPRESS_API, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: `
-        query getPosts($before: String, $after: String, $first: Int = null, $last: Int = null) {
-          category(id: "dGVybToxNw=") {
-            name
-            children (first: $first, last: $last, before: $before, after: $after){
-              nodes {
-                name
-                slug
-                id
-                description
-                categoryImages {
-                  categoryImage {
-                    sourceUrl
-                  }
-                }
-                courses(where: { orderby: { field: MENU_ORDER, order: ASC } }, first: 1) {
-                  edges {
-                    node {
-                      id
-                      uri
-                      title
-                      slug
-                      menuOrder
-                      link
-                    }
-                  }
-                }
-              }
-              pageInfo {
-                endCursor
-                hasNextPage
-                hasPreviousPage
-                startCursor
-              }
-            }
-          }
-        }
-      `,
-      variables: {
-        first: 18, // Use the updated first value for pagination
-        after: null, // Use the updated after value for pagination
-      },
-    }),
-  });
-
-  const data = await response.json();
-
-  const courses = data?.data?.category?.children?.nodes;
-  const pageInfo = data?.data?.category?.children?.pageInfo;
-
-  return { courses, pageInfo };
-}
-
-
 export default async function Courses() {
-
-  const { courses, pageInfo } = await getCourses();
-
   return (
+    <>
+      <div className='relative -z-10 w-0 h-0  top-[87px] left-[217px]'>
+        <Particles numberOfParticles={10} radius={60} opacity={0.2} color='black' left={0} />
+        <Particles numberOfParticles={30} radius={300} opacity={0.3} color='black' left={0} />
+        <Particles numberOfParticles={40} radius={380} opacity={0.2} color='black' left={0} />
+        <Particles numberOfParticles={50} radius={460} opacity={0.1} color='black' left={0} />
+        <Particles numberOfParticles={60} radius={540} opacity={0.05} color='black' left={0} />
+      </div>
+
+      <div className='flex justify-between  whitespace-nowrap text-slate-600 w-full h-40  text-5xl p-4 mb-6 overflow-hidden mt-8'  >
+        <span className="opacity-100 font-bold">Projects. </span>
+        <span className="opacity-10 hidden sm:block" >Projets. </span>
+        <span className="opacity-10 hidden sm:block">프로젝트. </span>
+        <span className="opacity-10 hidden md:block">项目. </span>
+        <span className="opacity-10 hidden lg:block">Проект. </span>
+      </div>
+
+      <div className='grid gap-1 grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))] '>
+        {projects.map((project) => (
+
+          <div key={project.slug} className=" p-2 rounded-xl  w-full h-full bg-white dark:bg-slate-800">
+            <Link href={`/blg/${project.slug}`} className="flex flex-col justify-between border border-gray-200 rounded-lg hover:shadow-lg transition-shadow duration-300 p-6 w-full h-full">
+              <h3 className="font-bold text-xl mb-2">{project.meta.title}</h3>
+              <p className="text-sm text-gray-500">{project.meta.publishedOn}</p>
+              <p className="py-4">{project.meta.abstract}</p>
+              <div className='flex flex-wrap gap-2 mb-4'>
+                {project.meta.tags.map((tag) => (
+                  <span key={tag} className="bg-gray-200 text-gray-800 text-xs font-semibold px-2 py-1.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <p className="text-indigo-600 hover:text-indigo-800 font-semibold cursor-pointer">Read more →</p>
+            </Link>
+          </div>
+
+        ))}
+
+      </div>
 
 
-    <div className='grid gap-1 grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))] '>
-      {projects.map((project) => (
 
-        <div key={project.slug} className=" p-2 rounded-xl  w-full h-full bg-white dark:bg-slate-800">
-          <Link href={`/blg/${project.slug}`} className="flex flex-col justify-between border border-gray-200 rounded-lg hover:shadow-lg transition-shadow duration-300 p-6 w-full h-full">
-            <h3 className="font-bold text-xl mb-2">{project.meta.title}</h3>
-            <p className="text-sm text-gray-500">{project.meta.publishedOn}</p>
-            <p className="py-4">{project.meta.abstract}</p>
-            <div className='flex flex-wrap gap-2 mb-4'>
-              {project.meta.tags.map((tag) => (
-                <span key={tag} className="bg-gray-200 text-gray-800 text-xs font-semibold px-2 py-1.5 rounded dark:bg-gray-700 dark:text-gray-300">
-                  {tag}
-                </span>
-              ))}
-            </div>
-            <p className="text-indigo-600 hover:text-indigo-800 font-semibold cursor-pointer">Read more →</p>
-          </Link>
-        </div>
-
-      ))}
-
-    </div>
+    </>
 
 
 

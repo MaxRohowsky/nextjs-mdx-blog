bf6ca1c3481c0bb64997c78e3bfc815004baef79
max <68344959+codewmax@users.noreply.github.com>
2024-07-21 22:05:55 +0200

    Refactor image lazy loading and update MDX components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -20,16 +20,16 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     a: ({ children, ...props }) => <a className="text-blue-500 hover:text-blue-600 underline" {...props}>{children}</a>,
     img: (props) => (
       <Image
-        height={800}
-        width={800}
-        className='rounded-sm'
+        width={400}
+        height={400}
+        className='mx-auto shadow-md rounded-sm'
         {...(props as ImageProps)}
       />
     ),
-    ul: ({ children }) => <ul className="list-disc pl-5 text-gray-600 my-4">{children}</ul>,
-    ol: ({ children }) => <ol className="list-decimal pl-5 text-gray-600 my-4">{children}</ol>,
+    ul: ({ children }) => <ul className="list-disc pl-5 my-4">{children}</ul>,
+    ol: ({ children }) => <ol className="list-decimal pl-5 my-4">{children}</ol>,
     li: ({ children }) => <li className="mb-2">{children}</li>,
-    blockquote: ({ children }) => <blockquote className="border-l-4 border-gray-200 pl-4 italic text-gray-600 my-4">{children}</blockquote>,
+    blockquote: ({ children }) => <blockquote className="border-l-4 border-gray-200 pl-4 italic  my-4">{children}</blockquote>,
     caption: ({ children }) => <caption className="text-center text-red-400 text-sm my-2">{children}</caption>,
 
     Caption: ({ children }) => (
@@ -37,9 +37,9 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ),
 
     Note: ({ children }) => (
-      <div className="bg-blue-50 border-l-[3px] border-blue-500 p-4 relative rounded-sm my-5">
+      <div className="bg-blue-50 dark:bg-blue-700 border-l-[3px] border-blue-500 p-4 relative rounded-sm my-8">
         <div className="absolute top-0 left-0 transform -translate-y-1/2 translate-x-[-17px]">
-          <Info className='bg-white text-blue-500 rounded-full p-1' size={32} />
+          <Info className='bg-white dark:bg-slate-900 dark:text-white  text-blue-500  rounded-full p-1' size={32} />
         </div>
         {children}
       </div>
@@ -47,9 +47,9 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
 
 
     Warning: ({ children }) => (
-      <div className="bg-red-50 border-l-[3px] border-red-500 p-4 relative rounded-sm my-5">
+      <div className="bg-red-50 dark:bg-red-700 border-l-[3px] border-red-500 dark:border-red-300 p-4 relative rounded-sm my-8">
         <div className="absolute top-0 left-0 transform -translate-y-1/2 translate-x-[-17px]">
-          <Flame className='bg-white text-red-500 rounded-full p-1' size={32} />
+          <Flame className='bg-white dark:bg-slate-900 text-red-500 rounded-full p-1' size={32} />
         </div>
         {children}
       </div>
